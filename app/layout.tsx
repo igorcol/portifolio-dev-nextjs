d2dcaa1af0b1c6643d1d19390233012c02f87031
igorcol <igor.colombini@gmail.com>
2025-03-12 17:33:45 -0300

    feat ScrollToTop & CustomCurstor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
+import AnimatedCursor from "react-animated-cursor";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop/ScrollToTop";
 
 const soraFont = Sora({
-  weight: ['100', '200', '300', '400',  '500', '600', '700', '800'],
-  subsets: ["latin"]
-})
-
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Igor Colombini Portfolio",
@@ -22,10 +23,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${soraFont.className} antialiased`} >
-        <ResponsiveNav/>
+      <body className={`${soraFont.className} antialiased`}>
+        <div className="hidden md:block">
+          <AnimatedCursor
+            innerSize={8}
+            outerSize={35}
+            innerScale={2}
+            outerScale={2}
+            outerAlpha={0}
+            innerStyle={{
+              backgroundColor: 'white'
+            }}
+            outerStyle={{
+              border: '3px solid white'
+            }}
+          />
+        </div>
+        <ResponsiveNav />
         {children}
         <Footer />
+        <ScrollToTop />
       </body>
     </html>
   );
